a7db0c5871bddfe3c833dcb9ef913e6215310b3c
Sambit Pradhan <sambitgulu@gmail.com>
2024-10-17 21:45:50 +0530

    base project

---
+++ src/main/java/com/example/SonarcloudDemo/SonarcloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SonarcloudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SonarcloudDemo/SonarcloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SonarcloudDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarcloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
